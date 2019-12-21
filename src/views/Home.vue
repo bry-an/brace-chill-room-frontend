7e00624c028004e56b4f499fd438748ea9644a3b
Bryan Yunis <bryany@booj.com>
2019-12-20 22:17:34 -0700

    inital commit, vue create and cleanup

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Brace Chill Room</h1>
+    <div>👩‍🎤👨‍🎤</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+  },
+};
+</script>
